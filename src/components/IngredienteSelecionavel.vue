e7dd93011038210316cb26dcba02bc133ca8e7c7
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2024-05-05 10:48:20 -0300

    Create IngredienteSelecionavel.vue

---
+++ src/components/IngredienteSelecionavel.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Tag from './Tag.vue';
+    export default {
+        components: { Tag },
+        props: {
+            ingrediente: { type: String, required: true }
+        }
+    }
+</script>
+<template>
+    <Tag :texto="ingrediente" />
+</template>
